0ddfa0b78f6ab2cc3e4582ddcd6df40b113e8439
nebulosa <developer1@recu.com.co>
2025-05-04 19:22:24 -0500

    add queries

--- Graphql/queries/queries.py
+++ Graphql/queries/queries.py
@@ -1,5 +1,11 @@
 import graphene
 from django.db import connections
+from news.models import News
+from photo.models import Photo
+# from ...news.models import News
+# from ...photo.models import Photo
+from ..schemas.shema_news import NewsShema
+from ..schemas.schema_photo import PhotoShema
 
 
 
@@ -9,6 +15,9 @@ class Query(graphene.ObjectType):
     # -----------------------------------------
 
     exist_id_investigation = graphene.Int()
+    all_news = graphene.List(NewsShema)
+    all_photos = graphene.List(PhotoShema)
+
 
     
     
@@ -16,3 +25,11 @@ class Query(graphene.ObjectType):
         with connections['secondary'].cursor() as cursor:
             cursor.execute("SELECT 1 FROM investigacion WHERE investigacion_id = %s LIMIT 1", [id])
             return cursor.fetchone() is not None
+
+    def resolve_all_news(root,info):
+
+        return News.objects.all()
+    
+    def resolve_all_photos(root, info):
+
+        return Photo.objects.all()
